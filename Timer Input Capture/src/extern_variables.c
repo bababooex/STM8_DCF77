d1679b9d3f2f05a966575152879caaab22c4375b
Adam Fučík <155400992+bababooex@users.noreply.github.com>
2026-06-24 12:36:09 +0200

    Delete Timer Input Capture directory

--- Timer Input Capture/src/extern_variables.c
+++
@@ -1,9 +0,0 @@
-#include "extern_variables.h"
-volatile uint8_t colonToggle;
-void visual_handler(void)//handle blink edge
-{
-		uint8_t col;
-		col = colonToggle;
-		if (col) HD1650_colonOn();
-		else     HD1650_colonOff();
-}
